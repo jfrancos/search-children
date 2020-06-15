3ba0112c4cb11147b316f8b1df35f49644014326
Justin Francos <justinfrancos@gmail.com>
2020-06-14 22:49:01 -0400

    Extracted Search from other project, converted to typescript

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
 
-import { ExampleComponent } from 'search-children'
-import 'search-children/dist/index.css'
+import Search, { Pierce } from 'search-children'
+// import 'search-children/dist/index.css'
+
+const DeeperEl = () => <div>qrweweweqttt</div>
+const DeepEl = () => <div>qwer<DeeperEl/></div>
+
+class SearchMe extends React.Component {
+  render() {
+    return <>
+        thas is deep
+    {this.props.children}
+    </>
+  }
+}
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <>
+    <Search searchValue='as'><div>asdf</div><div>asdf</div></Search>
+    <Search searchValue='we'><Pierce><DeepEl/><DeepEl/></Pierce>we</Search>
+    hello
+    <Search searchValue='as'><Pierce><SearchMe><div>asdf</div><div>asdf</div></SearchMe></Pierce></Search>
+
+    <Search searchValue=''></Search>
+    <Search searchValue='sd'>asdf</Search>
+
+  </>
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,83 @@
-import * as React from 'react'
-import styles from './styles.module.css'
+import React, {
+  isValidElement,
+  cloneElement,
+  createElement,
+  useEffect,
+  Children,
+  ReactNode,
+} from 'react'
+import Highlighter, { HighlighterProps } from "react-highlight-words";
 
-interface Props {
-  text: string
+type SearchProps = {
+  children?: ReactNode
+  onDone?: (itemsFound: number) => void
+  test?: boolean
+  searchValue: string
+  highlighterProps?: HighlighterProps
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+const shouldPierce = (child?: ReactNode): boolean =>
+  isValidElement(child) &&
+  typeof child.type === 'function' && child.type.name === 'Pierce'
+
+const Search = ({
+  children,
+  onDone,
+  test,
+  searchValue,
+  highlighterProps
+}: SearchProps): JSX.Element => {
+  let count = 0
+  const highlightTag = (
+    highlighterProps && highlighterProps.highlightTag
+  ) || 'mark'
+  const counterTag = ({ children }: { children: JSX.Element }) => {
+    count++
+    return createElement(highlightTag, null, children)
+  }
+
+  const Highlight = ({ child }: { child: string }) =>
+    <Highlighter
+      {...highlighterProps}
+      searchWords={[searchValue]}
+      textToHighlight={child}
+      highlightTag={counterTag}
+    />
+
+  const deepHighlight = (child?: ReactNode): any => {
+    if (test && typeof child === 'string') {
+      return <mark key={child}>{child}</mark>
+    }
+    if (typeof child === 'string') {
+      return <Highlight child={child} />
+    }
+    if (shouldPierce(child)) {
+      const piercedChildren = (child as JSX.Element).props.children
+      const pierceChildren =
+        (innerChild: JSX.Element) => {
+          const prototype = innerChild.type.prototype
+          if (prototype && prototype.isReactComponent) {
+            return new innerChild.type(innerChild.props).render()
+          } else {
+            return innerChild.type(innerChild.props)
+          }
+        }
+      return deepHighlight(Children.map(piercedChildren, pierceChildren))
+    }
+    if (isValidElement(child)) {
+      const props = child.props as { children: JSX.Element }
+      const children = deepHighlight(props.children)
+      return cloneElement(child, props, children)
+    }
+    if (Array.isArray(child)) {
+      return Children.map(child, deepHighlight)
+    }
+  }
+  useEffect(() => {
+    onDone && onDone(count)
+  })
+  return deepHighlight(children) || null
 }
+
+export const Pierce = ({ children }: { children?: any }) => children
+export default Search
